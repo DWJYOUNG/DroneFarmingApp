feb60a93e64bb57f724c3cf11cb8acf0480b79c4
Ian K Yuen <ikyuen@uab.edu>
2021-11-14 00:24:29 -0600

    Update PhysicalDroneAdapter.java -- small functionality to test whether it will actually fly at all

--- src/main/java/com/smartFarmApp/view/PhysicalDroneAdapter.java
+++ src/main/java/com/smartFarmApp/view/PhysicalDroneAdapter.java
@@ -3,8 +3,15 @@ package com.smartFarmApp.view;
 
 public class PhysicalDroneAdapter implements SimulatedDrone{
 
+    TelloDrone tello = new TelloDrone();
+
     @Override
     public void scanFarm() {
+        tello.takeoff;
+        tello.turnCW(90);
+        tello.flyForward(FARMWIDTH/PIXELS_TO_ONE_MODEL_FOOT*CENTIMETERS_PER_MODEL_FOOT);
+        tello.turnCCW(90);
+        tello.flyForward(FARMHEIGHT/PIXELS_TO_ONE_MODEL_FOOT*CENTIMETERS_PER_MODEL_FOOT);
     }
 
     @Override
